b90b9ee7ddd997768d808798627b1a97d1501474
Darian Springer <darianc.springer@gmail.com>
2015-01-31 22:51:20 +0000

    Darian Leaves

--- db/migrate/20150131214006_create_issue_links.rb
+++ db/migrate/20150131214006_create_issue_links.rb
@@ -1,11 +1,19 @@
 class CreateIssueLinks < ActiveRecord::Migration
   def change
-    create_table :issue_links do |t|
+    create_table :create_issue_links do |t|
 
-      t.references :issue, name: query_issue, index: true
-      t.references :issue, name: result_issue, index: true
-
+      t.integer :query_issue, index: true
+      t.integer :result_issue, index: true
       t.timestamps
     end
   end
 end
+
+
+class Issue < ActiveRecord::Base
+  has_many :issuelinks, :foreign_key => "query_issue", 
+  :class_name => "IssueLink"
+
+  has_many :resultIssuelinks, :through => :issuelinks
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131213246) do
+ActiveRecord::Schema.define(version: 20150131214006) do
 
   create_table "alexes", force: true do |t|
     t.string   "name"
@@ -38,6 +38,23 @@ ActiveRecord::Schema.define(version: 20150131213246) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "create_issue_links", force: true do |t|
+    t.integer  "query_issue"
+    t.integer  "result_issue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "event_issues", force: true do |t|
+    t.integer  "event_id"
+    t.integer  "issue_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "event_issues", ["event_id"], name: "index_event_issues_on_event_id"
+  add_index "event_issues", ["issue_id"], name: "index_event_issues_on_issue_id"
 
   create_table "events", force: true do |t|
     t.text     "description"
